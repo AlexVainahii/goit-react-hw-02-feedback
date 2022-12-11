042642fc31f2b649b0836399349b0de4be992f87
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-12-11 05:02:11 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,51 @@ import { Component } from 'react';
 import { FeedbackOptions } from './FeedbackOptions';
 import { Card } from './FeedbackOptions.styled';
 import { Statistics } from './Statistics';
-
+import { Percents, sumArrayValues } from './Function';
 export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
+  incrementGood = () => {
+    this.setState(state => ({ good: this.state.good + 1 }));
+  };
+  incrementNeutral = () => {
+    this.setState(state => ({ neutral: this.state.neutral + 1 }));
+  };
+  incrementBad = () => {
+    this.setState(state => ({ bad: this.state.bad + 1 }));
+  };
+  countTotalFeedback() {
+    return sumArrayValues(this.state);
+  }
+  countPositiveFeedbackPercentage() {
+    return Percents(this.state.good, this.countTotalFeedback());
+  }
   render() {
     return (
       <Card>
         <h2>Please leave feedback</h2>
-        <FeedbackOptions />
+        <FeedbackOptions
+          incrementGood={this.incrementGood}
+          incrementNeutrall={this.incrementNeutral}
+          incrementBad={this.incrementBad}
+        />
         <h2> Statistics</h2>
-        <Statistics good={} />
+        {this.countTotalFeedback === 0 ? (
+          <p>No Feedbaak given</p>
+        ) : (
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            countTotalFeedback={this.countTotalFeedback}
+            ountPositiveFeedbackPercentage={
+              this.countPositiveFeedbackPercentage
+            }
+          />
+        )}
       </Card>
     );
   }
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -2,13 +2,20 @@ import { Component } from 'react';
 
 export class Statistics extends Component {
   render() {
+    const {
+      good,
+      neutral,
+      bad,
+      countTotalFeedback,
+      countPositiveFeedbackPercentage,
+    } = this.props;
     return (
       <>
-        <p>Good: {this.state.good}</p>
-        <p>Neutral: {this.state.neutral}</p>
-        <p>Bad: {this.state.bad}</p>
-        <p>Total: {this.countTotalFeedback()}</p>
-        <p>Positive Feedback: {this.countPositiveFeedbackPercentage()}%</p>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {countTotalFeedback()}</p>
+        <p>Positive Feedback: {countPositiveFeedbackPercentage()}%</p>
       </>
     );
   }
